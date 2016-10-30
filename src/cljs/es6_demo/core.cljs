2215726af41be1f710fcc16aad909a1ee2f9740b
David Nolen <david.nolen@gmail.com>
2016-10-30 15:03:32 +0100

    wip

--- src/cljs/es6_demo/core.cljs
+++ src/cljs/es6_demo/core.cljs
@@ -1,4 +1,33 @@
 (ns es6-demo.core
-  (:require [React :as React]
-            [Components :as Components]))
+  (:require [React]
+            [Components]))
 
+(def ColorInput
+  (React/createClass
+    #js {:render
+         (fn []
+           (this-as this
+             (React/createElement "div" nil
+               (React/createElement "input"
+                 #js {:type "text"
+                      :className "center"
+                      :onChange (.. this -props -onChange)}))))}))
+
+(def Container
+  (React/createClass
+    #js {:getInitialState
+         (fn [] #js {:color ""})
+         :handleColorChange
+         (fn [event]
+           (this-as this
+             (.setState this #js {:color (.. event -target -value)})))
+         :render
+         (fn []
+           (this-as this
+             (React/createElement "div" nil
+               (React/createElement ColorInput #js {:onChange (. this -handleColorChange)})
+               (React/createElement Components/Circle #js {:color (.. this -state -color)}))))}))
+
+(React/render
+  (React/createElement Container)
+  (.getElementById js/document "app"))
